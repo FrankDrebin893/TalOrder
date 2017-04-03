530fb827c467e4bc47d55917f08555d191eed1cd
Rasmus Højte <rasmusvh@msn.com>
2017-04-03 22:47:18 +0200

    VS Config fixes

---
+++ TalOrder/src/TalOrder.cpp
@@ -0,0 +1,16 @@
+// TalOrder.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+int main()
+{
+	Coordinates coordinates(4, 4);
+	Piece piece(WHITE, KING, coordinates);
+	piece.printPiece();
+	std::cout << "Hello" << std::endl;
+	int i;
+	std::cin >> i;
+    return 0;
+}
--- TalOrder/src/board.cpp
+++ TalOrder/src/board.cpp
@@ -1,6 +1,5 @@
+#pragma once
 #include "stdafx.h"
-#include "board.h"
-
 
 Board::Board()
 {
