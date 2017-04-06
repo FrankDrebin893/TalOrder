b09e9c3a3a3fc93bfae23939b1e7c91a2e229e4f
Rasmus Højte <rasmusvh@msn.com>
2017-04-06 23:03:34 +0200

    Moved move_generation.cpp

---
+++ TalOrder/src/move_generation.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "move_generation.h"
+
+
+MoveGeneration::MoveGeneration()
+{
+}
+
+
+MoveGeneration::~MoveGeneration()
+{
+}
---
+++ TalOrder/src/move_generation.h
@@ -0,0 +1,8 @@
+#pragma once
+class MoveGeneration
+{
+public:
+	MoveGeneration();
+	~MoveGeneration();
+};
+
